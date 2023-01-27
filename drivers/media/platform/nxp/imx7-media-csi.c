4458069480a119d0c0025133d6dd8766631a7aa9
Laurent Pinchart <laurent.pinchart@ideasonboard.com>
2023-01-27 03:21:57 +0200

    media: imx: imx7-media-csi: Drop unneeded pad checks
    The subdev core guarantees that the .set_fmt() operation is always
    called with a valid pad. Drop the unneeded pad checks.
    
    Signed-off-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
    Tested-by: Adam Ford <aford173@gmail.com> #imx8mm-beacon-kit
    Acked-by: Rui Miguel Silva <rmfrfs@gmail.com>
    Tested-by: Martin Kepplinger <martin.kepplinger@puri.sm>

--- drivers/media/platform/nxp/imx7-media-csi.c
+++ drivers/media/platform/nxp/imx7-media-csi.c
@@ -1934,6 +1934,7 @@ static int imx7_csi_try_fmt(struct imx7_csi *csi,
 		sdformat->format.quantization = in_fmt->quantization;
 		sdformat->format.ycbcr_enc = in_fmt->ycbcr_enc;
 		break;
+
 	case IMX7_CSI_PAD_SINK:
 		*cc = imx7_csi_find_mbus_format(sdformat->format.code);
 		if (!*cc) {
@@ -1945,8 +1946,6 @@ static int imx7_csi_try_fmt(struct imx7_csi *csi,
 		if (sdformat->format.field != V4L2_FIELD_INTERLACED)
 			sdformat->format.field = V4L2_FIELD_NONE;
 		break;
-	default:
-		return -EINVAL;
 	}
 
 	imx7_csi_try_colorimetry(&sdformat->format);
@@ -1965,9 +1964,6 @@ static int imx7_csi_set_fmt(struct v4l2_subdev *sd,
 	struct v4l2_mbus_framefmt *fmt;
 	struct v4l2_subdev_format format;
 	int ret = 0;
-
-	if (sdformat->pad >= IMX7_CSI_PADS_NUM)
-		return -EINVAL;
 
 	mutex_lock(&csi->lock);
 
