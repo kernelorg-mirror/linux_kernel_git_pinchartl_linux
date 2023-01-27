2e978050a2878bd20d517c49437d0e936a84b612
Laurent Pinchart <laurent.pinchart@ideasonboard.com>
2023-01-27 03:21:57 +0200

    media: imx: imx7-media-csi: Drop unneeded src_sd check
    The .s_stream() and .link_validate() operations can't be called with a
    NULL src_sd, as subdev nodes are not registered before the async
    notifier completes. Remove the unneeded checks.
    
    Signed-off-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
    Tested-by: Adam Ford <aford173@gmail.com> #imx8mm-beacon-kit
    Acked-by: Rui Miguel Silva <rmfrfs@gmail.com>
    Tested-by: Martin Kepplinger <martin.kepplinger@puri.sm>

--- drivers/media/platform/nxp/imx7-media-csi.c
+++ drivers/media/platform/nxp/imx7-media-csi.c
@@ -1729,11 +1729,6 @@ static int imx7_csi_s_stream(struct v4l2_subdev *sd, int enable)
 
 	mutex_lock(&csi->lock);
 
-	if (!csi->src_sd) {
-		ret = -EPIPE;
-		goto out_unlock;
-	}
-
 	if (enable) {
 		ret = imx7_csi_init(csi);
 		if (ret < 0)
@@ -2023,9 +2018,6 @@ static int imx7_csi_pad_link_validate(struct v4l2_subdev *sd,
 	struct media_pad *pad = NULL;
 	unsigned int i;
 	int ret;
-
-	if (!csi->src_sd)
-		return -EPIPE;
 
 	/*
 	 * Validate the source link, and record whether the source uses the
